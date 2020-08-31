8364d2e5d27ac711fbd12f947c8494e63f6fa534
Caleb Jackson <CalebJ@Calebs-MacBook-Pro-2.local>
2020-08-30 22:21:41 -0500

    Initial commit "Let there be Code."

---
+++ app/src/main/java/com/example/hello_world_app/DisplayMessageActivity.java
@@ -0,0 +1,37 @@
+package com.example.hello_world_app;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.provider.MediaStore;
+import android.widget.MediaController;
+import android.widget.TextView;
+import android.widget.VideoView;
+
+public class DisplayMessageActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_display_message);
+
+        // Get the Intent that started this activity and extract the string
+        Intent intent = getIntent();
+        String message = intent.getStringExtra(MainActivity.EXTRA_MESSAGE);
+
+        // Capture the layout's TextView and set the string as its text
+        TextView textView = findViewById(R.id.textView);
+        textView.setText(message);
+
+        VideoView videoView = findViewById(R.id.videoView);
+        String videopath = "android.resource//" + getPackageName() + "/" + R.raw.video;
+        Uri uri = Uri.parse(videopath);
+        videoView.setVideoURI(uri);
+
+        MediaController mediaController = new MediaController(this);
+        videoView.setMediaController(mediaController);
+        mediaController.setAnchorView(videoView);
+    }
+}
